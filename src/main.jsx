4dd774c65cdb886e7b4d55b79d4eff707eb491cb
deepakmathur1107-cyber <deepakmathur1107@gmail.com>
2026-06-01 16:50:06 -0500

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+import { ClerkProvider } from '@clerk/clerk-react'
+import Router from './Router'
 import './index.css'
+
+const CLERK_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+if (!CLERK_KEY) throw new Error('VITE_CLERK_PUBLISHABLE_KEY not set in .env')
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ClerkProvider publishableKey={CLERK_KEY} afterSignOutUrl="/">
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ClerkProvider>
   </React.StrictMode>
 )
