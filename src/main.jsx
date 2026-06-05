a2c02ec370af97ca102ddf59516a7e8a9616f372
deepakmathur1107-cyber <deepakmathur1107@gmail.com>
2026-06-05 14:05:31 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { ClerkProvider } from '@clerk/clerk-react'
+/**
+ * src/main.jsx
+ *
+ * Entry point. Mounts Router which owns Clerk + BrowserRouter + all routes.
+ */
+ 
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import Router from './Router'
-import './index.css'
-
-const CLERK_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-if (!CLERK_KEY) throw new Error('VITE_CLERK_PUBLISHABLE_KEY not set in .env')
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ClerkProvider publishableKey={CLERK_KEY} afterSignOutUrl="/">
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-    </ClerkProvider>
-  </React.StrictMode>
+ 
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Router />
+  </StrictMode>
 )
+ 
