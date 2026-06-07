f673e719af00859968197bb43c742afa28940df0
deepakmathur1107-cyber <deepakmathur1107@gmail.com>
2026-06-07 01:45:55 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect, useRef, useCallback } from 'react'
+import { Link } from 'react-router-dom'
 import AppNav from './components/AppNav'
 import { DARK_THEME, LIGHT_THEME } from './theme'
 
