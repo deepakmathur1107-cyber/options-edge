3e0c941899f14cd388ce05f6e12f47f691603985
deepakmathur1107-cyber <deepakmathur1107@gmail.com>
2026-06-07 01:24:58 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { Link } from 'react-router-dom'
+import AppNav from './components/AppNav'
+import { DARK_THEME, LIGHT_THEME } from './theme'
 
 <Link to="/app/trades" style={{ color: '#00c8ff', fontSize: 11, textDecoration: 'none' }}>
   TRADE LOG
